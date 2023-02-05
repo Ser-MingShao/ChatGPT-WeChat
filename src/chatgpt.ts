77e9e67047201bc222a563a6ef538f9550363ffa
SerMs <67584847+Ser-MingShao@users.noreply.github.com>
2023-02-06 02:28:08 +0800

    Update chatgpt.ts

--- src/chatgpt.ts
+++ src/chatgpt.ts
@@ -4,7 +4,7 @@ import { ContactInterface, RoomInterface } from "wechaty/impls";
 import { Configuration, OpenAIApi } from "openai";
 
 // ChatGPT error response configuration
-const chatgptErrorMessage = "🤖️：AI机器人摆烂了，请稍后再试～";
+const chatgptErrorMessage = "🤖️：SerMs摆烂了，请稍后再试～";
 
 // ChatGPT model configuration
 // please refer to the OpenAI API doc: https://beta.openai.com/docs/api-reference/introduction
